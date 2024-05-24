3d4975d85de016dafd447fca525de9f3a523bc99
ScarletIgnis <101909146+ScarletIgnis@users.noreply.github.com>
2024-05-23 22:17:42 -0500

    Add model to the site

--- opterraria/admin.py
+++ opterraria/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Npc, Biome, Npc_Npc, NpcBiome,Biome_Result
+admin.site.register(Npc)
+admin.site.register(Biome)
+admin.site.register(Npc_Npc)
+admin.site.register(NpcBiome)
+admin.site.register(Biome_Result)
